4a3c933544eeefe5c7672a61fab3dfb80c02e022
Sebastian Malek <malek@malek.li>
2022-02-04 01:01:14 +0100

    move flatpak and dns setup to install.sh

--- dns/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl --output /etc/unbound/root.hints https://www.internic.net/domain/named.cache
--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-pacman -S - < pkglist.txt
+sudo pacman -S - < pkglist.txt
+
+flatpak install com.github.Eloston.UngoogledChromium
+#flatpak install org.signal.Signal
+#flatpak install org.videolan.VLC
+
+# unbound
+sudo curl --output /etc/unbound/root.hints https://www.internic.net/domain/named.cache
+
+
+echo 'name_servers="::1 127.0.0.1"
+resolv_conf_options="trust-ad"' | sudo tee /etc/resolvconf.conf
+
+sudo resolvconf -u
