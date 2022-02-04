cf63c558ef33b8456f6a068f013b19fb1c0c13bb
Sebastian Malek <malek@malek.li>
2022-02-04 01:14:12 +0100

    auto update root hints

--- install.sh
+++ install.sh
@@ -9,6 +9,12 @@ flatpak install com.github.Eloston.UngoogledChromium
 # unbound
 sudo curl --output /etc/unbound/root.hints https://www.internic.net/domain/named.cache
 
+# auto update root hints
+sudo cp roothints.service /etc/systemd/system/roothints.service
+sudo cp roothints.timer /etc/systemd/system/roothints.timer
+sudo systemctl daemon-reload
+sudo systemctl enable --now roothints.timer
+
 sudo cp resolvconf.conf /etc/resolvconf.conf
 sudo resolvconf -u
 
