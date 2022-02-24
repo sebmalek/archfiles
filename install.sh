3bca5416d1ee33a5b6df49b0f82f69e099eaacbb
Sebastian Malek <malek@malek.li>
2022-02-24 22:55:30 +0100

    clean up postinstall script

--- install.sh
+++ install.sh
@@ -2,22 +2,20 @@
 
 pacman -S - < pkglist.txt
 
-#usermod -a -G wheel malek
-
 systemctl enable gdm.service
 systemctl enable NetworkManager.service
 
 # unbound
-sudo curl --output /etc/unbound/root.hints https://www.internic.net/domain/named.cache
+curl --output /etc/unbound/root.hints https://www.internic.net/domain/named.cache
 
 # auto update root hints
-sudo cp roothints.service /etc/systemd/system/roothints.service
-sudo cp roothints.timer /etc/systemd/system/roothints.timer
-sudo systemctl daemon-reload
-sudo systemctl enable --now roothints.timer
+cp roothints.service /etc/systemd/system/roothints.service
+cp roothints.timer /etc/systemd/system/roothints.timer
+systemctl daemon-reload
+systemctl enable --now roothints.timer
 
-sudo cp resolvconf.conf /etc/resolvconf.conf
-sudo resolvconf -u
+cp resolvconf.conf /etc/resolvconf.conf
+resolvconf -u
 
-sudo cp unbound.conf /etc/unbound/unbound.conf
-sudo systemctl restart unbound.service
+cp unbound.conf /etc/unbound/unbound.conf
+systemctl restart unbound.service
